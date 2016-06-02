b2d62666b716d36d45899bd7ecfbf64b121451fd
crewstyle <ach@takeatea.com>
2016-06-02 19:25:12 +0200

    add Snippet custom post type

---
+++ resources/controllers/posttypes/SnippetPosttype.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AthenaTheme\Controllers\Posttypes;
+
+use GetOlympus\Field;
+use GetOlympus\Hera\Posttype\Controller\Posttype as HeraPosttype;
+
+class SnippetPosttype extends HeraPosttype
+{
+    protected $slug = 'snippet';
+
+    public function __construct()
+    {
+        // Parent constructor
+        parent::__construct();
+
+        // Update args
+        $this->args = [
+            'capability_type' => 'post',
+            'has_archive' => true,
+            'hierarchical' => false,
+            'menu_icon' => 'dashicons-editor-code',
+            'public' => true,
+            'query_var' => true,
+            'show_ui' => true,
+            'menu_position' => 5,
+
+            'supports' => ['title'],
+            'taxonomies' => [],
+        ];
+
+        // Update labels
+        $this->labels = [
+            'name' => __('Snippets', OL_TPL_DICTIONARY),
+            'singular_name' => __('Snippet', OL_TPL_DICTIONARY),
+        ];
+
+        // Update fields
+        $this->fields = [
+            Field\Code::build('code', [
+                'title' => __('Code', OL_TPL_DICTIONARY),
+            ]),
+        ];
+    }
+}
